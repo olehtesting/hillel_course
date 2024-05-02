b4c108daa4c1e395398289658426ee116b9aa30e
Oleh Mykhalevych <o.mihalevich@gmail.com>
2024-05-02 21:39:16 +0300

    Homework 5_3

--- lesson_5/homework 5_1.py
+++ lesson_5/homework 5_1.py
@@ -3,19 +3,23 @@ import keyword
 
 
 def is_valid_variable_name(name):
+
     if name[0].isdigit():
         return False
 
     if any(char.isupper() for char in name):
         return False
 
-    if any(char in string.punctuation.replace("_", "") for char in name):
+    if any(char != '_' and (char.isspace() or char in string.punctuation) for char in name):
         return False
 
     if name in keyword.kwlist:
         return False
 
-    if name.count("_") > 1:
+    if len(name) > 1 and all(char == '_' for char in name):
+        return False
+
+    if " " in name:
         return False
 
     return True

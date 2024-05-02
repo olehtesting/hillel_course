bb425d5338c0f289e8d72e2b89da6cb505a50e0c
Oleh Mykhalevych <o.mihalevich@gmail.com>
2024-05-02 18:04:00 +0300

    Homework 5_3

--- lesson_5/homework 5_1.py
+++ lesson_5/homework 5_1.py
@@ -3,27 +3,23 @@ import keyword
 
 
 def is_valid_variable_name(name):
-    # Перевірка чи починається з цифри
     if name[0].isdigit():
         return False
 
-    # Перевірка чи містить великі літери
     if any(char.isupper() for char in name):
         return False
 
-    # Перевірка на наявність пробілів та знаків пунктуації
-    if any(char in string.punctuation and char != '_' for char in name):
+    if any(char in string.punctuation.replace("_", "") for char in name):
         return False
 
-    # Перевірка на імена ключових слів
     if name in keyword.kwlist:
+        return False
+
+    if name.count("_") > 1:
         return False
 
     return True
 
 
-# Запит введення користувача
-user_input = input("Введіть ім'я змінної: ")
-
-# Виведення результату перевірки
+user_input = input("Введіть рядок: ")
 print(is_valid_variable_name(user_input))

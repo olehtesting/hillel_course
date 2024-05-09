e47fd094cc44a4bb0c08633b22f46df197999ae9
Oleh Mykhalevych <o.mihalevich@gmail.com>
2024-05-09 17:12:45 +0300

    Homework 7_4

---
+++ lesson_7/homework 7_3.py
@@ -0,0 +1,23 @@
+def second_index(text, some_str):
+    # Знаходимо індекс першого входження шуканого рядка
+    first_index = text.find(some_str)
+
+    # Якщо перше входження не знайдено, повертаємо None
+    if first_index == -1:
+        return None
+
+    # Знаходимо індекс другого входження шуканого рядка, починаючи з позиції після першого входження
+    second_index = text.find(some_str, first_index + 1)
+
+    # Якщо друге входження не знайдено, повертаємо None
+    if second_index == -1:
+        return None
+
+    return second_index
+
+# Перевірка
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
+print('ОК')

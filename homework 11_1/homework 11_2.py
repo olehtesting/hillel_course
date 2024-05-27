9acc4c9b766cef436300142045c556801533ac44
Oleh Mykhalevych <o.mihalevich@gmail.com>
2024-05-27 16:40:14 +0300

    Homework 11_2

---
+++ homework 11_1/homework 11_2.py
@@ -0,0 +1,20 @@
+from typing import Generator
+
+def generate_cube_numbers(end: int) -> Generator[int, None, None]:
+    n = 2
+    while True:
+        cube = n ** 3
+        if cube >= end:
+            return
+        yield cube
+        n += 1
+
+if __name__ == "__main__":
+    from inspect import isgenerator
+
+    gen = generate_cube_numbers(1)
+    assert isgenerator(gen) == True, 'Test0'
+    assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+    assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+    assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729], '10 у кубі це 1000'
+    print('Ok')
